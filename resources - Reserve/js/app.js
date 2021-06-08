3c0c144942f3edd9c2c2b36788dc8e017c168ba5
Gears Point <80081771+gearspoint@users.noreply.github.com>
2021-06-08 08:34:14 +0500

    extra folder deleted
    extra folder deleted

--- resources - Reserve/js/app.js
+++
@@ -1,6 +0,0 @@
-// require('./bootstrap');
-import { createApp } from 'vue';
-import { createRouter } from 'vue-router'
-createApp.use(createRouter)
-import App from './components/App.vue'
-createApp(App).mount("#app")
